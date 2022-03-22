ac4ec74ccd2c07900d839198a44744cfca1280cc
Rus-ka <rrkaziev@gmail.com>
2022-03-23 00:50:35 +0300

    firebase

--- mini-app/src/index.js
+++ mini-app/src/index.js
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
+//import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
 
+// firebase.initializeApp({
+//   apiKey: "AIzaSyAbvcOWlcKxgIOGMVdtNG8FvLsuPh3Pkz8",
+//   authDomain: "kanban-b2737.firebaseapp.com",
+//   projectId: "kanban-b2737",
+// });
+
+//const process = require('process');
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
+// firebase.analytics();
+// console.log(firebase);
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
